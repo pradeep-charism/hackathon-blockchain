d59c216b7461e1f8e65fb8622c1403a273de09c3
pradeep-charism <pradeep.charism@gmail.com>
2019-06-27 03:24:01 +0800

    BLockchain hackathon - Bank Ssi lookup

--- contracts/SsiService .sol
+++ contracts/SsiService .sol
@@ -4,12 +4,21 @@ contract SsiStore {
     function GetSsiFor(string memory name) public returns (string memory, string memory, string memory, string memory, string memory);
 }
 
+contract PublicKeysStore {
+    function GetPublicKeyFor(string memory name) public returns (string memory);
+}
+
 contract SsiService
 {
-    function lookupSsi(string memory name) public returns (uint)
+    function LookupSsi(string memory name) public returns (string memory, string memory, string memory, string memory, string memory)
     {
         SsiStore store = SsiStore(0xEd27042914B46ea48F1433FA026E3f67aB7408a2);
         return store.GetSsiFor(name);
+    }
 
+    function LookupPublicKey(string memory name) public returns (string memory)
+    {
+        PublicKeysStore store = PublicKeysStore(0xEd27042914B46ea48F1433FA026E3f67aB7408a2);
+        return store.GetPublicKeyFor(name);
     }
 }
